93013cbda3f2c5dcedf2595eec446be598327c21
adsheikh <adeelnayyersheikh@gmail.com>
2019-05-02 17:28:33 +0400

    Initial commit

---
+++ src/webparts/graphConsumer/GraphConsumerWebPart.ts
@@ -0,0 +1,62 @@
+import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart, IPropertyPaneConfiguration, PropertyPaneChoiceGroup } from '@microsoft/sp-webpart-base';
+import * as strings from 'GraphConsumerWebPartStrings';
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+import { ClientMode } from './components/ClientMode';
+import GraphConsumer from './components/GraphConsumer';
+import { IGraphConsumerProps } from './components/IGraphConsumerProps';
+
+export interface IGraphConsumerWebPartProps {
+  clientMode: ClientMode;
+}
+
+export default class GraphConsumerWebPart extends BaseClientSideWebPart<IGraphConsumerWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IGraphConsumerProps> = React.createElement(
+      GraphConsumer,
+      {
+        clientMode: this.properties.clientMode,
+        context: this.context,
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneChoiceGroup('clientMode', {
+                  label: strings.ClientModeLabel,
+                  options: [
+                    { key: ClientMode.aad, text: "AadHttpClient" },
+                    { key: ClientMode.graph, text: "MSGraphClient" },
+                  ]
+                }),
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/graphConsumer/components/IGraphConsumerProps.ts
@@ -0,0 +1,8 @@
+import { WebPartContext } from '@microsoft/sp-webpart-base';
+
+import { ClientMode } from './ClientMode';
+
+export interface IGraphConsumerProps {
+  clientMode: ClientMode;
+  context: WebPartContext;
+}
